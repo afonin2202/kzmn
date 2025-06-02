195f7bd079090f22d77be421999dca6eb971f368
afonin2202 <angelstarkme5@gmail.com>
2025-06-02 06:50:17 -0400

    Create unit_test.py

---
+++ unit_test.py
@@ -0,0 +1,13 @@
+from app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello'
+
+def test_not_found():
+    client = app.test_client()
+    response = client.get('/nonexistent')
+    assert b'Page not found' in response.data
+    assert response.status_code == 404
